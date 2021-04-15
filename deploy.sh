f566617694352dd510a55cdc2905aaafcb9c5707
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-04-15 16:31:31 +0100

    HOCS-2659 : Go back to set variable default with =

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ export KUBE_SERVER="https://kube-api-notprod.notprod.acp.homeoffice.gov.uk"
 export KUBE_TOKEN=${KUBE_TOKEN}
 export VERSION=${VERSION}
 
-export RUN_CONFIG_NUM_MESSAGES=${RUN_CONFIG_NUM_MESSAGES:-1}
-export RUN_CONFIG_COMPLAINT_TYPE=${RUN_CONFIG_COMPLAINT_TYPE:-""}
+export RUN_CONFIG_NUM_MESSAGES=${RUN_CONFIG_NUM_MESSAGES=1}
+export RUN_CONFIG_COMPLAINT_TYPE=${RUN_CONFIG_COMPLAINT_TYPE=""}
 
 export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt"
 
