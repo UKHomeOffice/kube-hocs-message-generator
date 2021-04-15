82fe78a8d1bef366ec3da4916649be2ebfb71bd2
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-04-15 15:35:52 +0100

    HOCS-2659 : yaml formatting issue, and fix unbound variable

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ export KUBE_TOKEN=${KUBE_TOKEN}
 export VERSION=${VERSION}
 
 export RUN_CONFIG_NUM_MESSAGES=${RUN_CONFIG_NUM_MESSAGES:-1}
-export RUN_CONFIG_COMPLAINT_TYPE=${RUN_CONFIG_COMPLAINT_TYPE}
+export RUN_CONFIG_COMPLAINT_TYPE=${RUN_CONFIG_COMPLAINT_TYPE:-""}
 
 export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt"
 
