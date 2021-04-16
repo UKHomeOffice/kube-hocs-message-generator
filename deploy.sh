3adc4fdc042cc2fd0b73fd5e085fb71989d36eda
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-04-16 12:15:57 +0100

    HOCS-2659 : Comment sleep and shorten

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ export KUBE_NAMESPACE=${ENVIRONMENT}
 export KUBE_SERVER="https://kube-api-notprod.notprod.acp.homeoffice.gov.uk"
 export KUBE_TOKEN=${KUBE_TOKEN}
 export VERSION=${VERSION}
-# get epoch time
+# ise epoch time to create a unique job
 export DEPLOYED_AT=`date +'%s'`
 
 export RUN_CONFIG_NUM_MESSAGES=${RUN_CONFIG_NUM_MESSAGES=1}
@@ -18,7 +18,8 @@ cd kd || exit 1
 kd --timeout 3m \
     -f message-generator-job.yaml 
 
-sleep 60
+echo "Sleeping 30 secs"
+sleep 30
 
 kd --timeout 3m \
     --delete \
