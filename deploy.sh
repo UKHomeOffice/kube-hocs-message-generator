441751d583921b4778ff72d92b1f084ac829a495
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-04-19 09:23:26 +0100

    HOCS-2659 : Use wait instead of sleep

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ export KUBE_NAMESPACE=${ENVIRONMENT}
 export KUBE_SERVER="https://kube-api-notprod.notprod.acp.homeoffice.gov.uk"
 export KUBE_TOKEN=${KUBE_TOKEN}
 export VERSION=${VERSION}
+export NAME="hocs-message-generator"
 # ise epoch time to create a unique job
 export DEPLOYED_AT=`date +'%s'`
 
@@ -18,8 +19,7 @@ cd kd || exit 1
 kd --timeout 3m \
     -f message-generator-job.yaml 
 
-echo "Sleeping 30 secs"
-sleep 30
+kubectl wait job/${NAME}-${DEPLOYED_AT} --for=condition=complete
 
 kd --timeout 3m \
     --delete \
